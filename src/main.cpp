06542192194a25a382577e74e0d2fb9b418a8d28
Evgeny Shulgin <izaronplatz@gmail.com>
2018-08-20 18:11:11 +0300

    Add Lua support

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
+extern "C" {
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+#include <luajit.h>
+}
+
 #include <iostream>
 
 int main() {
+    lua_State* state = luaL_newstate();
+
+    if (luaL_loadfile(state, "script.lua") || lua_pcall(state, 0, 0, 0)) {
+        std::cerr << "Error" << std::endl;
+        return 0;
+    }
+
+    lua_close(state);
+
     return 0;
 }
